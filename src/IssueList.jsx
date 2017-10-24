78e8677f7224fb5eddba95cca88ed2279b112be8
Oscar-21 <austin.conder@outlook.com>
2017-10-23 23:26:49 -0400

    ifif

---
+++ src/IssueList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'isomorphic-fetch';
+
+class IssueList extends React.Component {
+  render() {
+    return (
+      <div>
+      test
+      </div>
+    );
+  }
+}
+
+
+
+export default IssueListt
---
+++ static/app.bundle.js
@@ -0,0 +1,11 @@
+webpackJsonp([0],{
+
+/***/ 139:
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: Error: Couldn't find preset \"env\" relative to directory \"C:\\\\Users\\\\austi\\\\Documents\\\\mystuff\"\n    at C:\\Users\\austi\\Documents\\mystuff\\spotify\\jm\\node_modules\\babel-core\\lib\\transformation\\file\\options\\option-manager.js:293:19\n    at Array.map (native)\n    at OptionManager.resolvePresets (C:\\Users\\austi\\Documents\\mystuff\\spotify\\jm\\node_modules\\babel-core\\lib\\transformation\\file\\options\\option-manager.js:275:20)\n    at OptionManager.mergePresets (C:\\Users\\austi\\Documents\\mystuff\\spotify\\jm\\node_modules\\babel-core\\lib\\transformation\\file\\options\\option-manager.js:264:10)\n    at OptionManager.mergeOptions (C:\\Users\\austi\\Documents\\mystuff\\spotify\\jm\\node_modules\\babel-core\\lib\\transformation\\file\\options\\option-manager.js:249:14)\n    at OptionManager.init (C:\\Users\\austi\\Documents\\mystuff\\spotify\\jm\\node_modules\\babel-core\\lib\\transformation\\file\\options\\option-manager.js:368:12)\n    at File.initOptions (C:\\Users\\austi\\Documents\\mystuff\\spotify\\jm\\node_modules\\babel-core\\lib\\transformation\\file\\index.js:212:65)\n    at new File (C:\\Users\\austi\\Documents\\mystuff\\spotify\\jm\\node_modules\\babel-core\\lib\\transformation\\file\\index.js:135:24)\n    at Pipeline.transform (C:\\Users\\austi\\Documents\\mystuff\\spotify\\jm\\node_modules\\babel-core\\lib\\transformation\\pipeline.js:46:16)\n    at transpile (C:\\Users\\austi\\Documents\\mystuff\\spotify\\jm\\node_modules\\babel-loader\\lib\\index.js:50:20)\n    at Object.module.exports (C:\\Users\\austi\\Documents\\mystuff\\spotify\\jm\\node_modules\\babel-loader\\lib\\index.js:175:20)");
+
+/***/ })
+
+},[139]);
+//# sourceMappingURL=app.bundle.js.map
